66d52e6469c7792b8f9622c38757c6e2f74f28e3
Saku Kitaoka <o8qqqq@gmail.com>
2017-09-19 20:27:17 +0900

    Initial Commit

---
+++ algorithm and design pattern/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algorithm and design pattern
+//
+//  Created by o8q on 2017/09/19.
+//  Copyright © 2017年 o8q. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
